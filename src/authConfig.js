e615d4136a148afa38ad40d960707fe5d28e0fea
Silpa Sivadas <sivadas@rhrk.uni-kl.de>
2021-11-18 21:24:52 +0530

    Init

---
+++ src/authConfig.js
@@ -0,0 +1,21 @@
+export const msalConfig = {
+    auth: {
+      clientId: "3d7b657e-3d14-4508-8500-16745099a977",
+      authority: "https://login.microsoftonline.com/cibuscell.com", // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
+      redirectUri: "http://localhost:3000",
+    },
+    cache: {
+      cacheLocation: "sessionStorage", // This configures where your cache will be stored
+      storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    }
+  };
+  
+  // Add scopes here for ID token to be used at Microsoft identity platform endpoints.
+  export const loginRequest = {
+   scopes: ["User.Read"]
+  };
+  
+  // Add the endpoints here for Microsoft Graph API services you'd like to use.
+  export const graphConfig = {
+      graphMeEndpoint: "https://graph.microsoft.com/v1.0/me"
+  };
